f126ba9f590cc9494ac32cfbb31e18704085026c
Laura Osier <laura.osier@gmail.com>
2012-08-27 19:24:12 -0500

    Fixed order issue. Still need to code it.

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,6 @@ class Order < ActiveRecord::Base
   attr_accessible :comments, :total_price, :total_qty,
   								:user_id
   
-  has_many :products, :through => order_items
+  has_many :products, :through => :order_items
   
 end
