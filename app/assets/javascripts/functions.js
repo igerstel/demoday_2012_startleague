f66eb3969a3c1425ae1040f7e2720baf36409ce4
Kenton Quatman <kentonquatman@gmail.com>
2012-09-04 15:58:13 -0500

    Fixed JavaScript for Home Page Slide Show
    Signed-off-by: Kenton Quatman <kentonquatman@gmail.com>

---
+++ app/assets/javascripts/functions.js
@@ -0,0 +1,20 @@
+var Site = window.Site || {};
+(function($) {
+	$(function() {
+		
+		// PLACE HOLDER
+		
+		$('input, textarea').placeholder();
+		
+		// SLIDE SHOW
+
+    $("#slider1").responsiveSlides({
+      auto: true,
+      pager: true,
+      speed: 800,
+      maxwidth: 600
+    });
+		  
+  //END FUNCTIONS  
+	});
+})(jQuery);
